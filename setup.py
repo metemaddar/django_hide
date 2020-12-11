0e8670ffcfc982835dd4958779078112f3c6ed63
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2020-12-11 03:51:56 +0330

    Lets move to pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_hide-metemaddar", # Replace with your own username
+    version="0.0.1",
+    author="Mohammad Etemaddar",
+    author_email="mohammad.etemaddar@gmail.com",
+    description="Django Hide, hides django from wappalizer by encrypting csrf_token.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/metemaddar/django_hide",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
