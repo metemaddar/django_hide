b677b4183e2f99e5bddc72ba437188a43815c683
Fatiima-Ezzahra <ou.fatimaezzahra@gmail.com>
2021-01-08 15:42:08 +0000

    Clean the code

--- django_hide/templatetags/django_hide.py
+++ django_hide/templatetags/django_hide.py
@@ -4,9 +4,13 @@ from django.utils.html import format_html
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def h_csrf_token(context):
+    """
+    Template Tag that changes the html format of csrf_token.
+    """
     csrf_field = signing.dumps('csrfmiddlewaretoken').partition(':')[0]
-    return format_html("<input type='hidden' name='{0}' value='{1}' />", csrf_field, context['csrf_token'])
-    # csrf_field = signing.dumps('csrfmiddlewaretoken').partition(':')[0]
-    # return value.replace('csrfmiddlewaretoken',csrf_field)
+    csrf_token = context['csrf_token']
+    new_format = format_html("<input type='hidden' name='{0}' value='{1}' />", csrf_field, csrf_token)
+    return new_format
