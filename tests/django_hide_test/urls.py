50a581e813d94aded917a608abfc725feae27717
Mohammad <mohammad@mouse.mouse.com>
2024-09-02 20:13:25 +0330

    Add tests for h_csrf_token tag

---
+++ tests/django_hide_test/urls.py
@@ -0,0 +1,23 @@
+"""
+URL configuration for django_hide_test project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from testing_app.views import django_hide_form_view
+
+urlpatterns = [
+    path('', django_hide_form_view, name='django_hide_form_view'),
+]
