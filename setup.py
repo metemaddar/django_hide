9fb3ec8cfb005ce63d107b57e76e3b8957e9b04c
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2024-09-03 18:03:07 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,37 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="django-hide", # Replace with your own username
-    version="0.0.3",
+    name="django-hide",
+    version="0.0.4",
     author="Mohammad Etemaddar",
     author_email="mohammad.etemaddar@gmail.com",
-    description="Django Hide, hides django from wappalizer by encrypting csrf_token.",
+    description="A Django package to obscure Django applications from detection tools like Wappalyzer by encrypting the CSRF token.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/metemaddar/django_hide",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Security",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.8",
+    install_requires=[
+        "Django>=3.2",  # Ensure compatibility with Django versions
+    ],
+    keywords="django security wappalyzer hide csrf",
+    project_urls={
+        "Documentation": "https://github.com/metemaddar/django_hide/blob/main/README.md",
+        "Source": "https://github.com/metemaddar/django_hide",
+        "Tracker": "https://github.com/metemaddar/django_hide/issues",
+    },
 )
